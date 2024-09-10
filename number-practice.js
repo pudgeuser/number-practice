d69efdc2beae56cb83e820a58968cbbb05681d76
John Bryan Lomuntad <pudgeuser259@gmail.com>
2024-09-10 19:25:11 +0800

    Add files via upload

---
+++ number-practice.js
@@ -0,0 +1,13 @@
+// Create a variable named firstFavNum and store your favorite number 
+let firstFavNum = 51;
+
+// Create a variable named secondFavNum and store your favorite number 
+let secondFavNum = 10;
+
+// perform arithmetic operations
+console.log("Addition:", firstFavNum + secondFavNum);
+console.log("Subtraction:", firstFavNum - secondFavNum);
+console.log("Multiflication:", firstFavNum * secondFavNum); 
+console.log("Division:", firstFavNum / secondFavNum);
+console.log("MOdulos:", firstFavNum % secondFavNum);
+console.log("Exponentiation (Power):", firstFavNum ** secondFavNum);
